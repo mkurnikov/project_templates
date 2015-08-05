e94ad7abbcdcef2aaecaa86b4df511edca1a8b17
mkurnikov <maxim.kurnikov@gmail.com>
2015-08-05 19:17:46 +0300

    pipeline function for main file

--- makeproject/templates/kaggle/src/main.py
+++ makeproject/templates/kaggle/src/main.py
@@ -7,7 +7,23 @@ import numpy as np
 from sklearn.pipeline import Pipeline, FeatureUnion
 
 from src.base import get_train_data_target
+import src.feature_sets as feature_sets
 import settings
+
+
+def get_feature_union():
+    pass
+
+
+def get_estimation_pipeline():
+    pass
+
+
+def get_overall_pipeline():
+    return Pipeline([
+        get_feature_union(),
+        get_estimation_pipeline()
+    ])
 
 
 if __name__ == '__main__':
